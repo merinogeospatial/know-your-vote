434b5bca2874912799f0fe1612a51770b455ca23
R <merinogeospatial@gmail.com>
2018-08-03 18:27:31 -0400

    init MVC structure

---
+++ routes/html-routes.js
@@ -0,0 +1,15 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+// html routes here
+
+};
