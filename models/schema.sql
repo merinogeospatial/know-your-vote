0f8b680d6460242e43c0bc98560930eea4b8ff53
R <merinogeospatial@gmail.com>
2018-08-04 20:26:21 -0400

    update shema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -5,6 +5,6 @@ CREATE DATABASE exampledb;
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
 
--- Adding database for Know Your Vote app
+-- Adding database for Know Your Vote app --
 DROP DATABASE IF EXISTS votedb;
 CREATE DATABASE votedb;
